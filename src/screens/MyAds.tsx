1ba6d18deb598a8fa07ec1e8b01997b157a6503d
killer-cf <costa.kilder@live.com>
2023-07-19 11:04:19 -0300

    my ads select

--- src/screens/MyAds.tsx
+++ src/screens/MyAds.tsx
@@ -1,8 +1,10 @@
 import { Header } from "@components/Header";
-import { VStack } from "native-base";
+import { Box, CheckIcon, HStack, Select, Text, VStack } from "native-base";
 import { Plus } from "phosphor-react-native";
+import { useState } from "react";
 
 export function MyAds() {
+  const [service, setService] = useState("");
   function handlePressPlus() {
     console.log("Plus clicado")
   }
@@ -15,6 +17,22 @@ export function MyAds() {
         iconRight={<Plus size={26} weight="bold" />}
         onPressIconRight={handlePressPlus}
       />
+
+      <HStack mt={6} justifyContent={"space-between"} w={"full"}>
+        <Text color={"gray.600"} fontSize={'sm'}>9 anúncios</Text>
+        <Box maxW="300">
+        <Select selectedValue={service} minWidth="200" accessibilityLabel="Choose Service" placeholder="Choose Service" _selectedItem={{
+        bg: "teal.600",
+        endIcon: <CheckIcon size="5" />
+      }} mt={1} onValueChange={itemValue => setService(itemValue)}>
+          <Select.Item label="UX Research" value="ux" />
+          <Select.Item label="Web Development" value="web" />
+          <Select.Item label="Cross Platform Development" value="cross" />
+          <Select.Item label="UI Designing" value="ui" />
+          <Select.Item label="Backend Development" value="backend" />
+        </Select>
+      </Box>
+      </HStack>
     </VStack>
   )
 }
