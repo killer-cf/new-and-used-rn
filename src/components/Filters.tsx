4fbc48948da3d82a91482e923a12cedda34a1ba1
killer-cf <costa.kilder@live.com>
2023-08-09 11:02:40 -0300

    fetch for filtered ads

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,18 +1,58 @@
-import { Checkbox, HStack, Heading, Pressable, Switch, Text, VStack, useTheme } from "native-base";
+import { Checkbox, HStack, Heading, Pressable, Switch, Text, VStack, useTheme, useToast } from "native-base";
 import { X } from "phosphor-react-native"
 import { FilterTag } from "./FilterTag";
-import { useState } from "react";
 import { Button } from "./Button";
+import { Controller, useForm } from "react-hook-form";
+import { PaymentMethodsType } from "@dtos/PaymentMethodDTO";
+import { AppError } from "@utils/AppError";
+import { api } from "@services/api";
 
 type Props = {
   onCloseModal: () => void
 }
 
-type FilterStateType = 'NOVO' | 'USADO'
+type FilterFormData = {
+  state: 'NOVO' | 'USADO'
+  accept_trade: boolean
+  payment_methods: PaymentMethodsType[]
+}
 
 export function Filters({ onCloseModal}: Props) {
+  const { control, handleSubmit, setValue, watch } = useForm<FilterFormData>({
+    values: {
+      state: 'USADO',
+      accept_trade: false,
+      payment_methods: []
+    }
+  })
+
+  const state = watch('state')
+
   const { colors } = useTheme()
-  const [ state, setState] = useState<FilterStateType>('NOVO')
+  const toast = useToast()
+
+  async function handleFilter({accept_trade, state, payment_methods}: FilterFormData) {
+    const params = {
+      accept_trade,
+      payment_methods,
+      is_new: state === 'NOVO' ? true : false,
+    }
+
+    try {
+      const response = await api.get('/products', { params })
+
+      console.log(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError  
+      const title = isAppError ? error.message : 'Não foi possível buscar pelos filtros. Tente novamente mais tarde'
+      
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
+  }
 
   return (
     <VStack bg={"gray.200"} p={6} flex={1}>
@@ -29,48 +69,60 @@ export function Filters({ onCloseModal}: Props) {
         Condição
       </Heading>
       <HStack pt={3}>
-        <FilterTag title="NOVO" mr={2} selected={state === 'NOVO'} onPress={() => setState('NOVO')} />
-        <FilterTag title="USADO" selected={state === 'USADO'} onPress={() => setState('USADO')}/>
+        <FilterTag title="NOVO" mr={2} selected={state === 'NOVO'} onPress={() => setValue('state', 'NOVO')} />
+        <FilterTag title="USADO" selected={state === 'USADO'} onPress={() => setValue('state', 'USADO')}/>
       </HStack>
 
       <Heading fontFamily={"heading"} fontSize={'sm'} mt={6} mb={3} color={"gray.600"} >
         Aceita troca?
       </Heading>
-      <Switch onTrackColor={"lightBlue.500"} />
+      <Controller 
+        name='accept_trade'
+        control={control}
+        render={({ field: { value, onChange }}) => (
+          <Switch onTrackColor={"lightBlue.500"} value={value} onValueChange={onChange}/>
+        )}
+      />
 
       <Heading fontFamily={"heading"} fontSize={'sm'} mt={6} mb={3} color={"gray.600"} >
         Meios de pagamento aceitos
       </Heading>
-      <Checkbox.Group flex={1} accessibilityLabel="selecione os meios de pagamento aceitos">
-        <HStack alignItems={"center"} mb={2}>
-          <Checkbox  value="ticket" accessibilityLabel="Boleto?" _checked={{backgroundColor: 'lightBlue.500', borderColor: 'lightBlue.500'}}/>
-          <Text ml={2} fontFamily={"body"} fontSize={"md"}>Boleto</Text>
-        </HStack>
-        
-        <HStack alignItems={"center"} mb={2}>
-          <Checkbox  value="pix" accessibilityLabel="Pix?" _checked={{backgroundColor: 'lightBlue.500', borderColor: 'lightBlue.500'}}/>
-          <Text ml={2} fontFamily={"body"} fontSize={"md"}>Pix</Text>
-        </HStack>
+      <Controller 
+        name='payment_methods'
+        control={control}
+        render={({ field: { value, onChange }}) => (
+          <Checkbox.Group defaultValue={[]} value={value} onChange={onChange} flex={1} accessibilityLabel="selecione os meios de pagamento aceitos">
+            <HStack alignItems={"center"} mb={2}>
+              <Checkbox  value="boleto" accessibilityLabel="Boleto?" _checked={{backgroundColor: 'lightBlue.500', borderColor: 'lightBlue.500'}}/>
+              <Text ml={2} fontFamily={"body"} fontSize={"md"}>Boleto</Text>
+            </HStack>
+            
+            <HStack alignItems={"center"} mb={2}>
+              <Checkbox  value="pix" accessibilityLabel="Pix?" _checked={{backgroundColor: 'lightBlue.500', borderColor: 'lightBlue.500'}}/>
+              <Text ml={2} fontFamily={"body"} fontSize={"md"}>Pix</Text>
+            </HStack>
 
-        <HStack alignItems={"center"} mb={2}>
-          <Checkbox  value="cash" accessibilityLabel="Dinheiro?" _checked={{backgroundColor: 'lightBlue.500', borderColor: 'lightBlue.500'}}/>
-          <Text ml={2} fontFamily={"body"} fontSize={"md"}>Dinheiro</Text>
-        </HStack>
+            <HStack alignItems={"center"} mb={2}>
+              <Checkbox  value="cash" accessibilityLabel="Dinheiro?" _checked={{backgroundColor: 'lightBlue.500', borderColor: 'lightBlue.500'}}/>
+              <Text ml={2} fontFamily={"body"} fontSize={"md"}>Dinheiro</Text>
+            </HStack>
 
-        <HStack alignItems={"center"} mb={2}>
-          <Checkbox  value="credit_card" accessibilityLabel="cartão de crédito?" _checked={{backgroundColor: 'lightBlue.500', borderColor: 'lightBlue.500'}}/>
-          <Text ml={2} fontFamily={"body"} fontSize={"md"}>Cartão de crédito</Text>
-        </HStack>
+            <HStack alignItems={"center"} mb={2}>
+              <Checkbox  value="card" accessibilityLabel="cartão de crédito?" _checked={{backgroundColor: 'lightBlue.500', borderColor: 'lightBlue.500'}}/>
+              <Text ml={2} fontFamily={"body"} fontSize={"md"}>Cartão de crédito</Text>
+            </HStack>
 
-        <HStack alignItems={"center"} mb={2}>
-          <Checkbox  value="deposit" accessibilityLabel="Deposito bancário?" _checked={{backgroundColor: 'lightBlue.500', borderColor: 'lightBlue.500'}}/>
-          <Text ml={2} fontFamily={"body"} fontSize={"md"}>Deposito bancário</Text>
-        </HStack>
-      </Checkbox.Group>
+            <HStack alignItems={"center"} mb={2}>
+              <Checkbox  value="deposit" accessibilityLabel="Deposito bancário?" _checked={{backgroundColor: 'lightBlue.500', borderColor: 'lightBlue.500'}}/>
+              <Text ml={2} fontFamily={"body"} fontSize={"md"}>Deposito bancário</Text>
+            </HStack>
+          </Checkbox.Group>
+        )}
+      />
 
       <HStack mb={5}>
         <Button text={'Resetar filtros'} flex={1} mr={3} buttonColor="white-gray"/>
-        <Button text={'Aplicar filtros'} flex={1} buttonColor="gray"/>
+        <Button text={'Aplicar filtros'} onPress={handleSubmit(handleFilter)} flex={1} buttonColor="gray"/>
       </HStack>
     </VStack>
   )
